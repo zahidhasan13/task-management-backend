03db09a69e7219549ff93ca5fd507c7f42d442a0
zahidhasan13 <zahidhasanofficial13@gmail.com>
2025-11-05 11:11:19 +0600

    issue fix

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -20,7 +20,7 @@ export default function LoginPage() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:3000/api/auth/login", {
+      const res = await fetch("/api/auth/login", {
         method: "POST",
         credentials: "include",
         headers: { "Content-Type": "application/json" },
